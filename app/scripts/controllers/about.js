b12003f0d59953e67833d430c59f9d9c94d4dace
Akhilesh Ojha <akowebd@gmail.com>
2016-09-27 22:52:42 +0530

    App setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name astrostoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the astrostoreApp
+ */
+angular.module('astrostoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
